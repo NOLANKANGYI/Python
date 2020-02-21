0b500aee2bfa0a1be90fca049b876f3c3e930162
Nolan <nolankangyi@icloud.com>
2020-02-22 00:18:45 +0100

    creat pascal triangle

---
+++ pscal_triangle.py
@@ -0,0 +1,17 @@
+def triangles():
+	first,second= [1], [1,1]
+	yield first
+	yield second
+	while True:
+		second= [1]+[second[i]+second[i+1] for i in range(len(second)-1)]+[1]
+		yield second
+n=int(input("The height of the triangle: "))
+m=0
+results=[]
+for t in triangles():
+	results.append(t)
+	m=m+1
+	if m==n:
+		break
+for t in results:
+	print(t)
